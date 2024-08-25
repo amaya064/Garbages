2ae77851d624b372c3187a45cd487c0e31e99aa0
amaya064 <amayabandara0604@gmail.com>
2024-08-26 00:06:43 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose
@@ -21,3 +22,4 @@ app.listen (3000,()=>{
 );
 
 
+app.use('/api/user', userRouter);
